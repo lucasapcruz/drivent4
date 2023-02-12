069145d0a5e89c400f5f444e6dea349fdb6250c0
Lucas Cruz <lucas.apcruz@gmail.com>
2023-02-12 20:40:57 -0300

    fix: status now present in notFoundError

--- src/errors/not-found-error.ts
+++ src/errors/not-found-error.ts
@@ -1,8 +1,12 @@
 import { ApplicationError } from "@/protocols";
+import httpStatus from "http-status";
 
-export function notFoundError(): ApplicationError {
+export function notFoundError(): ApplicationErrorWithStatus {
   return {
     name: "NotFoundError",
     message: "No result for this search!",
+    status: httpStatus.NOT_FOUND
   };
 }
+
+type ApplicationErrorWithStatus = ApplicationError & { status: number }
